82221ad67f1bcb4c48b8394cb1cc092fe154c557
chidi1 <chidi@lotusbetaanalytics.com>
2017-06-20 16:15:25 +0100

    my current files on calculator

---
+++ Lotus calculator.js
@@ -0,0 +1,15 @@
+*{
+			border-radius: 5px;
+		}
+		.calc{
+			border: 1px solid #ee3cff;
+			width: 212px;
+			margin: auto;
+		}
+		input{
+			width:48px;
+			margin: 1px;
+		}
+		#display{
+			width: 199px
+		}
